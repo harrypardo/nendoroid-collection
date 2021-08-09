d295d243271831526a2a33330a63f4f7367e9f70
Harry Pardo <harrypardo108@gmail.com>
2021-08-09 23:40:39 +0800

    test husky

--- nendoroid-collection/src/App.tsx
+++ nendoroid-collection/src/App.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -16,9 +16,6 @@ function App() {
           </button>
         </p>
         <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -27,7 +24,7 @@ function App() {
           >
             Learn React
           </a>
-          {' | '}
+          {" | "}
           <a
             className="App-link"
             href="https://vitejs.dev/guide/features.html"
@@ -39,7 +36,7 @@ function App() {
         </p>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
